afb38e66480e32e96ea00e5f00b12672c98e74ab
Caio Alcântara <caiocomputers@gmail.com>
2019-03-29 23:36:04 -0300

    Initial setup

---
+++ KindleBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KindleBook
+//
+//  Created by Caio Alcântara on 29/03/19.
+//  Copyright © 2019 Red Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
